d6753774d3d41344d9005e95219cfdca357d774f
haaaaaaaaaaaaaaaaaaam <afterworkham@gmail.com>
2025-08-12 12:58:11 -0700

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,4 @@
 const postcards = [
   { sketch: 'https://editor.p5js.org/davepagurek/sketches/7YRHVfZRl', canUseInMedia: true },
+ { sketch: 'https://editor.p5js.org/davepagurek/sketches/n3QKOu_vL', canUseInMedia: true },
 ];
